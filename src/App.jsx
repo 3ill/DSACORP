20ddcce99d95a0385cfbef140a19cdf07b2b2bbc
3illBaby <thrillanon@outlook.com>
2023-08-24 19:11:40 +0100

    Added public and src dir

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center self-center padding">
+      <h1 className="font-extrabold font-mono text-4xl text-black text-center">
+        IN DEVELOPMENT, CHECK BACK LATER
+      </h1>
+    </div>
+  );
+};
+
+export default App;
