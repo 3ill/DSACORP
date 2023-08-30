204357d4610cb72cb6c5b71d5a2a6744dcd238b5
3illBaby <thrillanon@outlook.com>
2023-08-30 10:00:41 +0100

    Added control

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,26 @@
-const Button = ({ title, background, textColor, onClick, href, animate }) => {
+const Button = ({
+  title,
+  background,
+  textColor,
+  onClick,
+  href,
+  animate,
+  disabled,
+}) => {
   return (
-    <a
-      href={href}
-      className={`${background}  ${animate} border border-secondary rounded-[20px] flex items-center px-5 py-3 shadow-md group`}
-      onClick={onClick}
-    >
-      <h1
-        className={`${textColor} font-Azeret font-semibold max-sm:text-[13px] md:text-[16px] lg:text-[18px] group-hover:pulse-animation `}
+    <button disabled={disabled}>
+      <a
+        href={href}
+        className={`${background}  ${animate} border border-secondary rounded-[20px] flex items-center px-5 py-3 shadow-md group`}
+        onClick={onClick}
       >
-        {title}
-      </h1>
-    </a>
+        <h1
+          className={`${textColor} font-Azeret font-semibold max-sm:text-[13px] md:text-[16px] lg:text-[18px] group-hover:pulse-animation `}
+        >
+          {title}
+        </h1>
+      </a>
+    </button>
   );
 };
 
