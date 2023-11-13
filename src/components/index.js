f016ee871172f049f1354ffeaceb69cf37af7ae0
3illBaby <thrillanon@outlook.com>
2023-11-13 19:03:27 +0100

    Exported componets and tweaked style

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Button from './Button';
 import Download from './Download';
 import LearnNav from './LearnNav';
 import ReactTweet from './react-tweet';
+import StarCanvas from './canvas/stars';
 
-export { Nav, LearnNav, Button, Download, ReactTweet };
+export { Nav, LearnNav, Button, Download, ReactTweet, StarCanvas };
